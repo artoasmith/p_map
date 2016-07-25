7f87c3f1b7f5c337d64710934d6195fe841f2c7c
Alex Hotsaliyk <artoasmith@gmail.com>
2016-07-25 17:26:27 +0300

    pokemon entity

---
+++ pokemon/src/PokemonBundle/Base/UploaderEntity.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace PokemonBundle\Base;
+
+use Symfony\Component\Yaml\Parser;
+
+class UploaderEntity
+{
+    public function documentRoot(){
+        $yaml = new Parser();
+        $a = $yaml->parse(file_get_contents(__DIR__ . '/../../../../app/config/params.yml'));
+        return (isset($a['document_root'])?$a['document_root']:'');
+    }
+
+    /**
+     * @return string
+     */
+    public function defaultFolderPath(){
+        return '/upload/defaultFolderPath/';
+    }
+
+    public function clearOldUpload($type){
+        if(!$type)
+            return;
+
+        $file = $this->documentRoot().$this->defaultFolderPath().$this->{'get'.$type}();
+        @unlink($file);
+    }
+
+    public function upload($type, $fileurl = ''){
+        if (empty($fileurl)) {
+            if (null === $this->{'file' . $type}) {
+                return;
+            }
+
+            $filename = md5(time() . rand(5, 100)) . '.' . pathinfo($this->{'file' . $type}->getClientOriginalName(), PATHINFO_EXTENSION);
+            $filePath = $this->defaultFolderPath();
+            $filename = str_replace('..', '.', $filename);
+
+
+            $this->clearOldUpload($type);
+
+            $file = $this->{'file' . $type};
+            $newfile = $this->documentRoot() . $filePath . $filename;
+
+            if (\copy($file, $newfile)) {
+                $this->{'set' . $type}($filename);
+            }
+        } else {
+            $arr = @file($fileurl);
+            if (!empty($arr)) {
+
+                $pathinfo = pathinfo($fileurl);
+                if (strpos($pathinfo['extension'], '?') !== false)
+                    $filename = md5(time() . rand(5, 100)) . '.' . substr($pathinfo['extension'], 0, strpos($pathinfo['extension'], '?'));
+                else
+                    $filename = md5(time() . rand(5, 100)) . '.' . $pathinfo['extension'];
+
+                $filePath = $this->defaultFolderPath();
+                $filename = str_replace('..', '.', $filename);
+
+                if (strpos($fileurl, '?') !== false) {
+                    $filename = substr($filename, 0, strpos($fileurl, '?'));
+                }
+
+                $this->clearOldUpload($type);
+
+                $newfile = $this->documentRoot() . $filePath . $filename;
+                if (\copy($fileurl, $newfile)) {
+                    $this->{'set' . $type}($filename);
+                }
+            }
+        }
+    }
+}
---
+++ pokemon/src/PokemonBundle/Entity/Pokemon.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace PokemonBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use PokemonBundle\Base\UploaderEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Pokemon
+ *
+ * @ORM\Table(name="pokemon")
+ * @ORM\Entity(repositoryClass="PokemonBundle\Repository\PokemonRepository")
+ */
+class Pokemon extends UploaderEntity
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="image", type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @Assert\File(maxSize="10000000")
+     */
+    private $fileImage;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Pokemon
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     * @return Pokemon
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string 
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFileImage()
+    {
+        return $this->fileImage;
+    }
+
+    /**
+     * @param mixed $fileImage
+     */
+    public function setFileImage($fileImage)
+    {
+        $this->fileImage = $fileImage;
+    }
+
+    /**
+     * @return string
+     */
+    public function defaultFolderPath(){
+        return '/upload/pokemonImages/';
+    }
+}
