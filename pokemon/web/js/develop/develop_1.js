6bdc293a8398eae5434262745661e618fdb95eb9
Artem <artem.garmatyk@gmail.com>
2016-08-16 14:25:21 +0300

    add fix

--- pokemon/web/js/develop/develop_1.js
+++ pokemon/web/js/develop/develop_1.js
@@ -130,14 +130,90 @@ t:{try{Ft=Yt&&Yt.f("util");break t}catch(t){}Ft=void 0}var Ht=Ft&&Ft.isArrayBuff
 "\xee":"i","\xef":"i","\xd1":"N","\xf1":"n","\xd2":"O","\xd3":"O","\xd4":"O","\xd5":"O","\xd6":"O","\xd8":"O","\xf2":"o","\xf3":"o","\xf4":"o","\xf5":"o","\xf6":"o","\xf8":"o","\xd9":"U","\xda":"U","\xdb":"U","\xdc":"U","\xf9":"u","\xfa":"u","\xfb":"u","\xfc":"u","\xdd":"Y","\xfd":"y","\xff":"y","\xc6":"Ae","\xe6":"ae","\xde":"Th","\xfe":"th","\xdf":"ss"}),un=w({"&":"&amp;","<":"&lt;",">":"&gt;",'"':"&quot;","'":"&#39;","`":"&#96;"}),on=w({"&amp;":"&","&lt;":"<","&gt;":">","&quot;":'"',"&#39;":"'",
 "&#96;":"`"}),fn=N();typeof define=="function"&&typeof define.amd=="object"&&define.amd?(Vt._=fn, define(function(){return fn})):Gt?((Gt.exports=fn)._=fn,Kt._=fn):Vt._=fn}).call(this);
 
-$(document).ready(function(){
+$(window).on( "scroll", function(){
 
+    if (  $('.ban-topper-placeholder').length != 0 ){    
+
+        if ( $(window).scrollTop() < ( $('.ban-topper-placeholder').height() - 200 + $('header').height() ) ) {
+
+            var topS = $('header').height() - $(window).scrollTop() ;
+
+            $('.ban-topper').css('top', topS );
+
+        } else {
+
+            var topS = 200 - $('.ban-topper-placeholder').height()  ;
+            $('.ban-topper').css('top', topS  );
+
+        }
+    }
 });
 
-$(window).load(function(){
 
-});
+function googleMapContact(mapWrap){
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(cordXcontact ,cordYcontact);
+        var myOptions = {
+            zoom: 5,
+            center: myLatlng,
+             styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}],
+            disableDefaultUI: false, //без управляющих елементов
+            mapTypeId: google.maps.MapTypeId.ROADMAP, // SATELLITE - снимки со спутника,
+            zoomControlOptions: {
+               position: google.maps.ControlPosition.LEFT_BOTTOM // позиция слева внизу для упр елементов
+            }
+        }
+        var map = new google.maps.Map(document.getElementById(mapWrap), myOptions);
 
-$(window).resize(function(){
+        var contentString = '<div class="marker-test">'+googleTextcontact+'</div>';
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
 
-});
+
+        /*маркер на svg*/
+       // var SQUARE_PIN = 'M0-48c-9.8 0-17.7 7.8-17.7 17.4 0 15.5 17.7 30.6 17.7 30.6s17.7-15.4 17.7-30.6c0-9.6-7.9-17.4-17.7-17.4z'
+        //больше - http://map-icons.com/
+        /*/маркер на svg*/
+
+        var image = 'images/pock.png';   // иконка картинкой
+
+        
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            animation: google.maps.Animation.DROP, // анимация при загрузке карты
+            icon: image //  иконка картинкой
+           /* icon: {                               //маркер на svg
+                path: SQUARE_PIN,
+                fillColor: '#fff',
+                fillOpacity: 0.7,
+                strokeColor: '#FF3232',
+                strokeWeight: 5
+            },*/
+        });
+
+        /*анимация при клике на маркер*/
+        marker.addListener('click', toggleBounce);
+        function toggleBounce() {
+          if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+          } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+          }
+        }
+        /*/анимация при клике на маркер*/
+
+        /*По клику открываеться инфоблок*/
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map,marker);
+        });
+
+    }
+    initialize();
+}
+
+if ( $('body').find('#map-contact').length == 1 ){
+    google.maps.event.addDomListener(window, "load", googleMapContact('map-contact'));
+}
+
